f47980df28c0041a462d39924c48a1388d6215cb
[dyad] <felipe.saraiva.quadros@gmail.com>
2026-03-16 16:29:28 -0400

    [dyad] Cadastro de contratos de Soja 24/25 no banco de dados via SQL - wrote 1 file(s)

--- update_db.sql
+++ update_db.sql
@@ -1,53 +1,45 @@
 -- 1. Garante que os armazéns citados existam no cadastro
 INSERT INTO public.armazens (nome)
 VALUES 
-  ('SIPAL LRV'),
-  ('KODIAK/ AGROFÉRTIL'),
-  ('SIPAL LRV/ AGROFERTIL'),
-  ('INPASA'),
   ('AMAGGI MATUPÁ'),
-  ('INPASA - 1º Pagamento'),
-  ('INPASA - 2º Pagamento'),
-  ('ADM DO BRASIL (N.S.H.)'),
   ('AMAGGI SINOP'),
-  ('SIPAL MATUPÁ'),
-  ('SIPAL CLÁUDIA')
+  ('ADM STA HELENA (AC GRÃOS)'),
+  ('SIPAL'),
+  ('AMAGGI LRV'),
+  ('GO AGRO ARMAZENS GERAIS LTDA')
 ON CONFLICT (nome) DO NOTHING;
 
--- 2. Limpa contratos de milho existentes para evitar duplicidade na carga
-DELETE FROM public.contratos WHERE safra_id = 'milho25';
+-- 2. Limpa contratos de soja 24/25 existentes para evitar duplicidade
+DELETE FROM public.contratos WHERE safra_id = 'soja2425';
 
--- 3. Insere os novos contratos de Milho
--- Nota: IDs com sufixos (-CST, -SLZ, etc) para permitir múltiplos registros do mesmo contrato
+-- 3. Insere os novos contratos de Soja 24/25
 INSERT INTO public.contratos (safra_id, numero, nome, volume_total, armazem_id)
 SELECT 
-  'milho25',
+  'soja2425',
   t.numero,
   t.nome,
   t.sacas,
   a.id
 FROM (
   VALUES 
-    ('290924M180', 'BARTER - Romancini', 14297, 'SIPAL LRV'),
-    ('SEMENTES-KODIAK', 'BARTER Sementes - Romancini', 5063, 'KODIAK/ AGROFÉRTIL'),
-    ('SEMENTES-SIPAL', 'BARTER Sementes - Romancini', 2118, 'SIPAL LRV/ AGROFERTIL'),
-    ('INSUMOS-SIPAL', 'BARTER Insumos - Romancini', 380, 'SIPAL LRV/ AGROFERTIL'),
-    ('ARR-INPASA', 'ARRENDAMENTO - Castanhal', 6000, 'INPASA'),
-    ('20240106900533-1', 'BARTER - Estrelinha', 14460, 'AMAGGI MATUPÁ'),
-    ('21960-CST-1', 'VENDA 1º Pgto - Castanhal', 27665, 'INPASA - 1º Pagamento'),
-    ('21960-SLZ-1', 'VENDA 1º Pgto - São Luiz', 11073, 'INPASA - 1º Pagamento'),
-    ('21960-CST-2', 'VENDA 2º Pgto - Castanhal', 21262, 'INPASA - 2º Pagamento'),
-    ('28614', 'VENDA - Castanhal', 536, 'INPASA'),
-    ('290925M216', 'VENDA - Romancini', 10000, 'SIPAL LRV'),
-    ('1334P50494M', 'VENDA - São Luiz', 23522, 'ADM DO BRASIL (N.S.H.)'),
-    ('GRAO-DIRETO-AS', 'VENDA Grão Direto - São Luiz', 694, 'AMAGGI SINOP'),
-    ('GRAO-DIRETO-AM-1', 'VENDA Grão Direto - Estrelinha', 6500, 'AMAGGI MATUPÁ'),
-    ('GRAO-DIRETO-AM-2', 'VENDA Grão Direto - Estrelinha', 1000, 'AMAGGI MATUPÁ'),
-    ('GRAO-DIRETO-AM-3', 'VENDA Grão Direto - Estrelinha', 2550, 'AMAGGI MATUPÁ'),
-    ('COOP-LRV-1', 'VENDA Cooperativa - Romancini', 12000, 'SIPAL LRV'),
-    ('COOP-SM', 'VENDA Cooperativa - Estrelinha', 935, 'SIPAL MATUPÁ'),
-    ('COOP-SC', 'VENDA Cooperativa - São Luiz', 7308, 'SIPAL CLÁUDIA'),
-    ('COOP-LRV-2', 'VENDA Cooperativa - Romancini', 9173, 'SIPAL LRV'),
-    ('COOP-LRV-3', 'VENDA Cooperativa - Romancini', 3000, 'SIPAL LRV')
+    ('9058-8634', 'BARTER - Amaggi Matupá', 5754, 'AMAGGI MATUPÁ'),
+    ('9058-8633', 'BARTER - Amaggi Sinop', 18000, 'AMAGGI SINOP'),
+    ('ARR-PC', 'ARREND. + PLANT. + TRATOR + INSUMOS (Paulo Cezar)', 11577, 'ADM STA HELENA (AC GRÃOS)'),
+    ('SERV-D', 'SERVIÇO (Dieison)', 1032, 'ADM STA HELENA (AC GRÃOS)'),
+    ('290925M51', 'VENDA - Romancini', 3000, 'SIPAL'),
+    ('290925M69', 'VENDA - Romancini', 3000, 'SIPAL'),
+    ('ILDIANA', 'VENDA - Estrelinha', 3000, 'AMAGGI MATUPÁ'),
+    ('20240105500994-1', 'VENDA - Romancini', 18000, 'AMAGGI LRV'),
+    ('1334P50762S', 'VENDA - Castanhal', 20000, 'ADM STA HELENA (AC GRÃOS)'),
+    ('51/2025', 'VENDA - Castanhal', 5916.04, 'GO AGRO ARMAZENS GERAIS LTDA'),
+    ('290925M282', 'VENDA - Romancini', 3471, 'SIPAL'),
+    ('960732', 'VENDA - Castanhal (Ildo)', 1000, 'ADM STA HELENA (AC GRÃOS)'),
+    ('ILDO-MAT-1', 'VENDA - Estrelinha (Ildo)', 5013, 'AMAGGI MATUPÁ'),
+    ('ILDO-SIN-1', 'VENDA - São Luiz (Ildo)', 10339.67, 'AMAGGI SINOP'),
+    ('ILDO-SIN-2', 'VENDA - São Luiz (Ildo)', 671.17, 'AMAGGI SINOP'),
+    ('960734', 'VENDA - Castanhal (Ildo)', 3948.15, 'ADM STA HELENA (AC GRÃOS)'),
+    ('960737', 'VENDA - Castanhal (Ildiana)', 393.40, 'ADM STA HELENA (AC GRÃOS)'),
+    ('ILDO-MAT-2', 'VENDA - Estrelinha (Ildo)', 3018, 'AMAGGI MATUPÁ'),
+    ('2025-01-055-01160', 'VENDA - Romancini', 526.62, 'AMAGGI LRV')
 ) AS t(numero, nome, sacas, armazem_nome)
 JOIN public.armazens a ON a.nome = t.armazem_nome;
